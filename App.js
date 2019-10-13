1feb95ea9cdc8844e698e18b2e45d5ea73d211f9
haidi <haidinurhadinata@gmail.com>
2019-10-13 13:09:07 +0800

    membuat gopay

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
 import React, {Component} from 'react';
 import {
-  SafeAreaView,
   StyleSheet,
-  ScrollView,
   View,
   Text,
-  StatusBar,
+  TextInput,
+  Image,
 } from 'react-native';
 
 type Props = {};
@@ -14,22 +13,47 @@ class App extends Component<Props> {
   render(){
     return(
       <View style={styles.container}>
-        <View style={styles.body}></View>
+        {/* search bar */}
+        <View style={styles.header}>
+          <View style={{position:"relative", flex: 1}}>
+            <TextInput placeholder="what do you want to eat ? " style={styles.inputSearch} />
+            <Image source={require('./assets/icon/search.png')} style={styles.iconSearch} />
+          </View>
+          <View style={styles.positionPromo}>
+            <Image source={require('./assets/icon/promo.png')} />
+          </View>
+        </View>
+        {/* gopay */}
+        <View style={{marginHorizontal: 17, marginTop: 4}}>
+          <View style={styles.infoGopay}>
+            <Image source={require('./assets/icon/gopay.png')} />
+            <Text style={styles.nominalGopay}>Rp. 50.000</Text>
+          </View>
+          <View></View>
+        </View>
+        <View style={styles.body}>
+          
+        </View>
         <View style={styles.buttonNavigation}>
-          <View style={{backgroundColor:"red", flex: 1}}>
-            <Text>Home</Text>
+          <View style={styles.buttonMenu}>
+            <Image style={styles.buttonMenuIcon} source={require('./assets/icon/home-active.png')} />
+            <Text style={{color:"#43AB4A", ...styles.buttonMenuText}}>Home</Text>
           </View>
-          <View style={{backgroundColor:"pink", flex: 1}}>
-            <Text>Orders</Text>
+          <View style={styles.buttonMenu}>
+            <Image style={styles.buttonMenuIcon} source={require('./assets/icon/order.png')} />
+            <Text style={styles.buttonMenuText}>Orders</Text>
           </View>
-          <View style={{backgroundColor:"purple", flex: 1}}>
-            <Text>Help</Text>
+          <View style={styles.buttonMenu}>
+            <Image style={styles.buttonMenuIcon} source={require('./assets/icon/help.png')} />
+            <Text style={styles.buttonMenuText}>Help</Text>
           </View>
-          <View style={{backgroundColor:"brown", flex: 1}}>
-            <Text>Inbox</Text>
+          <View style={styles.buttonMenu}>
+            <Image style={styles.buttonMenuIcon} source={require('./assets/icon/inbox.png')} />
+            <Text style={styles.buttonMenuText}>Inbox</Text>
           </View>
-          <View style={{backgroundColor:"green", flex: 1}}>
-            <Text>Account</Text>
+          <View style={styles.buttonMenu}>
+            <Image style={styles.buttonMenuIcon} source={require('./assets/icon/account.png')} />
+            <Text style={styles.buttonMenuText}>Account</Text>
           </View>
         </View>
       </View>
@@ -43,16 +67,67 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  body:{
-    flex: 1,
-    backgroundColor:"blue"
+  header:{
+    flexDirection: "row",
+    marginHorizontal: 17,
+    paddingTop: 15,
   },
+  body:{
+    flex:1,
+    marginHorizontal: 17,
+    flexDirection: "row"
+  },
+  iconSearch:{
+    position: "absolute",
+    top: 5,
+    left: 12
+  },
+  inputSearch: {
+    borderWidth: 1,
+    borderColor: "#E8E8E8",
+    borderRadius: 25,
+    height: 40,
+    fontSize: 13,
+    paddingLeft: 45,
+    paddingRight: 15,
+    marginRight: 5,
+    backgroundColor:"white"
+  },
+  positionPromo:{
+    width: 35,
+    alignItems: "center",
+    justifyContent: "center",
+    // backgroundColor: "blue"
+  },
+  infoGopay:{
+    flexDirection: "row", 
+    justifyContent:"space-between",
+    backgroundColor: '#2A5DB3',
+    borderTopLeftRadius: 4,
+    borderTopRightRadius: 4,
+    padding: 14,
+  },
+  nominalGopay:{
+    fontSize: 17,
+    fontWeight: "bold",
+    color:"white"
+  },  
+  // styles menu below
   buttonNavigation:{
-    backgroundColor: "yellow",
     height: 54,
     flexDirection:"row"
   },
  buttonMenu:{
-   backgroundColor: "red"
+    flex: 1,
+    alignItems: "center", // posisi tengah secara vertical
+    justifyContent:"center" // posisi tengah secara horizontal
+ },
+ buttonMenuText:{
+  fontSize: 10,
+  marginTop: 4
+ }, 
+ buttonMenuIcon:{
+   width: 26,
+   height: 26,
  }
 })
