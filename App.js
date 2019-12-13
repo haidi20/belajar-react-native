20972d07338809659898ae2256f20e20deb89270
haidi <haidinurhadinata@gmail.com>
2019-12-13 16:59:38 +0800

    selesai tutorial 3

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import FindMe from './components/FindMe';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.header}>
+        <Text style={styles.boldText} >
+          Hello, world!
+        </Text>
+      </View>
+      <View style={styles.body}>
+        <Text style={styles.boldText}>Lorem ipsum dolor sit amet.</Text>
+        <Text>Lorem ipsum dolor sit amet.</Text>
+        <Text>Lorem ipsum dolor sit amet.</Text>
+      </View>
     </View>
   );
 }
@@ -16,4 +26,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header: {
+    backgroundColor: 'pink',
+    padding: 20,
+  },
+  boldText: {
+    fontWeight: 'bold',
+  },
+  body: {
+    backgroundColor: 'yellow',
+    padding: 20,
+    fontWeight: 'bold'
+  }
 });
---
+++ components/FindMe.js
@@ -0,0 +1,43 @@
+import React, {Component} from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location';
+import * as Permissions from 'expo-permissions';
+
+async function alertIfRemoteNotificationsDisabledAsync() {
+    const { status, permissions } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== 'granted') {
+        alert('gagal akses ijin lokasi');
+    }else{        
+        let location = await Location.getCurrentPositionAsync({});
+
+        return JSON.stringify(location)
+    }
+}  
+
+export default class FindMe extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            location: 'masih kosong'
+        }
+    }
+
+    async componentDidMount(){
+        let location = await alertIfRemoteNotificationsDisabledAsync();
+        console.log(location)
+
+        this.setState({
+            location: location
+        })
+    }
+
+    render(){
+        return(
+            <View>
+                <Text>find me</Text>
+                <Text>lokasi {this.state.location}</Text>
+            </View>
+        )
+    }
+}
