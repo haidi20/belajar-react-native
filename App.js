b9955a48b7933e35826e2a90193a94c434b84821
haidi <haidinurhadinata@gmail.com>
2019-12-14 08:36:06 +0800

    tutorial 8 sudah selesai

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native';
 import FindMe from './components/FindMe';
 
 export default function App() {
@@ -13,6 +13,13 @@ export default function App() {
     {name: 'mungkidi', id: '7'},
   ])
 
+  const pressHandler = (id) => {
+    // alert(id);
+    setPeople((prevPeople) => {
+      return prevPeople.filter(person =>  person.id != id);
+    })
+  }
+
   return (
     <View style={styles.container}>
       <FlatList 
@@ -20,7 +27,9 @@ export default function App() {
         keyExtractor={(item) => item.id }
         data={people}
         renderItem={({item}) => (
-          <Text style={styles.item}>{item.name}</Text>
+          <TouchableOpacity onPress={() => pressHandler(item.id)}>
+            <Text style={styles.item}>{item.name}</Text>
+          </TouchableOpacity>
         )}
       />
 
