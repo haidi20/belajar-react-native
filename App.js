8fc211198232808cef060f63b4ba23ce46319945
haidi <haidinurhadinata@gmail.com>
2019-12-20 21:47:16 +0800

    tutorial 11 (to do list 3) sudah selesai

--- App.js
+++ App.js
@@ -3,9 +3,10 @@ import { StyleSheet, Text, View, FlatList } from 'react-native';
 import FindMe from './components/FindMe';
 import Header from './components/Header';
 import TodoItem from './components/TodoItem';
+import AddTodo from './components/addTodo';
 
 export default function App() {
-  const [todos, sestTodos] = useState([
+  const [todos, setTodos] = useState([
     {text: 'buy coffe', key: '1'},
     {text: 'buy milk', key: '2'},
     {text: 'buy wafer', key: '3'},
@@ -14,14 +15,27 @@ export default function App() {
   const pressHandler = (key) => {
     sestTodos((prevTodos) => {
       return prevTodos.filter(todo => todo.key != key);
-    })
+    });
   }
+
+  const submitHandler = (text) => {
+    setTodos((prevTodos) => {
+        return [
+            {text: text, key: Math.random().toString()},
+            ...prevTodos
+        ]
+    })
+
+  }
+
 
   return (
     <View style={styles.container}>
       <Header />
       <View style={styles.content}>
-        {/* to form */}
+        <AddTodo 
+          submitHandler={submitHandler}
+        />
         <View style={styles.list}>
           <FlatList
             data={todos}
